8214991db39fa0cf3c4087112fde34327005a7b7
loete <14996758+loete@users.noreply.github.com>
2020-08-17 20:52:53 +0200

    added workbox-sw

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log('hellp from service worker ...');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
